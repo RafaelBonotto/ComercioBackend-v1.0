5126d0f690aa6c14e34855bfb7ad419c24f0afc1
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-08-28 21:55:42 -0300

    Update PagamentoConexao.cs

--- Pagamentos/Infra/Pagamentos.Infra/Conexao/PagamentoConexao.cs
+++ Pagamentos/Infra/Pagamentos.Infra/Conexao/PagamentoConexao.cs
@@ -1,9 +1,25 @@
-﻿using Pagamentos.Infra.Conexao.Interfaces;
+﻿using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using Pagamentos.Infra.Conexao.Interfaces;
 
 namespace Pagamentos.Infra.Conexao
 {
     public class PagamentoConexao : IPagamentoConexao
     {
+        private readonly IConfiguration _config;
+        private readonly string _connectionString;
 
+        public PagamentoConexao(IConfiguration config)
+        {
+            _config = config;
+            _connectionString = _config.GetConnectionString("Comerciodb");
+        }
+
+        public async Task<MySqlConnection> GetConnectionAsync()
+        {
+            MySqlConnection connection = new(_connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
     }
 }
