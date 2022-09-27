3bbe6117f9367d68d83b4cfcb0bbb29178be4888
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-09-27 15:48:57 -0300

    Controller

---
+++ Pagamentos/Servicos/Pagamentos.Api/Controllers/PagamentosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pagamentos.Api.Controllers
+{
+    [Route("api/v1/pagamento")]
+    [ApiController]
+    public class PagamentosController : ControllerBase
+    {
+    }
+}
