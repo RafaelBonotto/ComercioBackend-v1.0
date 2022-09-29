21214d3d0ebe77d5df17c0d4cae215df4c77ff45
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-09-28 23:01:18 -0300

    Post

--- Pagamentos/Aplicacao/Pagamentos.Aplicacao/Handles/Interfaces/IInserirPagamentoHandle.cs
+++ Pagamentos/Aplicacao/Pagamentos.Aplicacao/Handles/Interfaces/IInserirPagamentoHandle.cs
@@ -1,7 +1,9 @@
-﻿namespace Pagamentos.Aplicacao.Handles.Interfaces
+﻿using Pagamentos.Dominio.Entidades;
+
+namespace Pagamentos.Aplicacao.Handles.Interfaces
 {
     public interface IInserirPagamentoHandle
     {
-
+        Task<int> PostAsync(Pagamento pagamento);
     }
 }
--- Pagamentos/Servicos/Pagamentos.Api/Controllers/PagamentosController.cs
+++ Pagamentos/Servicos/Pagamentos.Api/Controllers/PagamentosController.cs
@@ -1,7 +1,9 @@
 ﻿using Comum.Aplicacao.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Pagamentos.Aplicacao.Handles.Interfaces;
 using Pagamentos.Aplicacao.Request;
+using System.Globalization;
 using System.Text;
 
 namespace Pagamentos.Api.Controllers
@@ -10,6 +12,13 @@ namespace Pagamentos.Api.Controllers
     [ApiController]
     public class PagamentosController : ControllerBase
     {
+        private IInserirPagamentoHandle _inserirPagamentoHandle;
+
+        public PagamentosController(IInserirPagamentoHandle inserirPagamentoHandle)
+        {
+            _inserirPagamentoHandle = inserirPagamentoHandle;
+        }
+
         [HttpPost]
         public async Task<IActionResult> PostAsync([FromBody] PagamentoRequest req)
         {
@@ -21,8 +30,29 @@ namespace Pagamentos.Api.Controllers
 
                 return BadRequest(msg.ToString());
             }
-                
-            return Ok();
+            try
+            {
+                var pagamento = new Dominio.Entidades.Pagamento
+                {
+                    Ativo = 1,
+                    Data_criacao = DateTime.Now,
+                    Data_alteracao = DateTime.Now,
+                    Dt_vencimento = DateTime.ParseExact(req.DataVencimento, "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Dt_entrega = DateTime.ParseExact(req.DataEntrega, "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Valor = req.Valor,
+                    Num_parcela = req.NumeroParcela,
+                    Qtd_parcela = req.QtdParcela,
+                    Fornecedor_id = req.FornecedorId,
+                    NotaFiscal = int.Parse(req.NotaFiscal)
+                };
+                return Ok(await _inserirPagamentoHandle.PostAsync(pagamento));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            
+            
         }
     }
 }
