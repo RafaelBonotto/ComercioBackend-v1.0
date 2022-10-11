fdf92bd9ee3a4bcaec6d492d45df7b576a666de9
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-10-10 23:31:37 -0300

    Configuração da classe Program Usuario

--- Pagamentos/Servicos/Pagamentos.Api/Program.cs
+++ Pagamentos/Servicos/Pagamentos.Api/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Pagamentos.Aplicacao;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+ConfigureAuthentication(builder);
 
 // Add services to the container.
 
@@ -19,10 +23,31 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); 
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtKey").Value);
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
